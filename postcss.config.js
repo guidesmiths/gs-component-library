65ecad2b6a6012749aacaa8dc5e3f4febdf777cd
Gabriel Pfeffer <gabriel.pfeffer@guidesmiths.com>
2020-12-21 09:45:27 -0300

    feat: postcss packages and config

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    plugins: {
+      "postcss-import": {},
+      "postcss-flexbugs-fixes": {},
+      "postcss-preset-env": {
+        stage: 0,
+      },
+      "postcss-mixins": {},
+      "postcss-css-variables": {},
+      "postcss-nested": {},
+      "postcss-inherit": {},
+      "postcss-reporter": {},
+      "postcss-custom-properties": {},
+      "postcss-custom-media": {},
+      lost: {},
+    },
+   };
